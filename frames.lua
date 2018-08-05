51466250150d8d82fb934941b5a1834d8764e5ff
Alex Mikitik <github@mikitik.com>
2018-08-04 20:48:20 -0700

    Namespace the frame-related functions.

--- frames.lua
+++ frames.lua
@@ -3,13 +3,16 @@
 ---------------------------------------------------------------------
 
 local _, ns = ...
+ns.frames = {}
+
 local LSM = LibStub and LibStub:GetLibrary("LibSharedMedia-3.0", true)
+local frames = ns.frames
 
 
 -- 
 -- Decorate a Frame with a background and outline texture.
 -- 
-function ns:setBackdrop(frame)
+function frames.setBackdrop(frame)
   if not LSM then print("LibStub failed to load") return end
   if frame.background then return end
 
