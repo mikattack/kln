e87983957fd470387e3547a5d52cafe91aa773a4
Alex Mikitik <github@mikitik.com>
2018-08-11 05:08:56 -0700

    Refactor events into a standalone object.

--- events.lua
+++ events.lua
@@ -3,9 +3,8 @@
 ---------------------------------------------------------------------
 
 local _, ns = ...
-ns.events = {}
-
-local events = ns.events
+ns.lib.EventHandler = {}
+local EventHandler = ns.lib.EventHandler
 
 --[[
 This API is ONLY used for synthetic events. That is, it will not
@@ -13,19 +12,27 @@ recognize game events, nor pass any game event information by default.
 It is used only for triggering events pertinent to "kln" addons.
 --]]
 
-function events:Register(event, fn)
+function EventHandler:New()
+  handlers = {}
+  setmetatable(handlers, self)
+  self.__index = self
+  return handlers
+end
+
+
+function EventHandler:Register(event, fn)
   if string.find(event, ",") then
     ns:print("klnCore: Cannot register event '%s'", event)
     return
   end
-  if not events[event] then
+  if not self[event] then
     self[event] = {}
   end
   self[event][#events + 1] = fn
 end
 
 
-function events:Unregister(event, fn)
+function EventHandler:Unregister(event, fn)
   if not self[event] then return end
   for k, v in pairs(self[event]) do
     if v == fn then
@@ -35,16 +42,17 @@ function events:Unregister(event, fn)
 end
 
 
-function events:UnregisterAll(event)
-  if self[event] then
-    self[event] = nil
+function EventHandler:UnregisterAll(event)
+  if not self[event] then return end
+  for k, v in pairs(self[event]) do
+    self[event][k] = nil
   end
 end
 
 
-function events:Trigger(event, ...)
+function EventHandler:Trigger(event, ...)
   if not self[event] then return end
   for _, fn in pairs(self[event]) do
-    fn(...)
+    if type(fn) == "function" then fn(...) end
   end
 end
