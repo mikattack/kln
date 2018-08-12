de73c6835b72fcfb60775882b6d96c1f82a9565c
Alex Mikitik <github@mikitik.com>
2018-08-12 15:45:05 -0700

    Improve clarity of "onload". Include configuration hook.

--- onload.lua
+++ onload.lua
@@ -4,28 +4,54 @@
 
 local _, ns = ...
 
-ns.onload = CreateFrame("Frame", nil, UIParent)
-ns.onload:RegisterEvent("ADDON_LOADED")
-ns.onload:SetScript("OnEvent", function (self, event, addon, ...)
-  if addon ~= ns.addon_name then return end
-  if event == "ADDON_LOADED" then
-    -- Only need to set up once
-    self:UnregisterEvent('ADDON_LOADED')
-    self:SetScript("OnEvent", nil)
 
-    -- Session storage
-    _G.kln_profiles  = _G.kln_profiles or {}
-    _G.kln_character = _G.kln_character or {}
-
-    -- Initialize events
-    ns.events = ns.lib.EventHandler:New()
-
-    -- Initialize profile management
-    ns.profiles = ns.lib.ProfileManager:New(kln_profiles, kln_character)
-    if not ns.profiles:Current() then
-      character, realm = UnitName('player')
-      ns.profiles:Load(character)
-    end
-
-  end
+local Loader = CreateFrame("Frame", nil, UIParent)
+Loader:RegisterEvent("ADDON_LOADED")
+Loader:SetScript("OnEvent", function(self, event, ...)
+  return self[event] and self[event](self, event, ...)
 end)
+
+
+function Loader:ADDON_LOADED(event, addon, ...)
+  if addon ~= ns.addon_name then return end
+    
+  -- Session storage
+  _G.kln_profiles  = _G.kln_profiles or {}
+  _G.kln_character = _G.kln_character or {}
+
+  -- Initialize events
+  ns.events = ns.lib.EventHandler:New()
+
+  -- Initialize profile management
+  ns.profiles = ns.lib.ProfileManager:New(kln_profiles, kln_character)
+  if not ns.profiles:Current() then
+    character, realm = UnitName('player')
+    ns.profiles:Load(character)
+  end
+
+  -- Load configuration on demand
+  ns.events:Register("show_configuration", function(...)
+    LoadConfiguration()
+  end)
+
+  -- Cleanup
+  self:UnregisterEvent(event)
+  self:SetScript("OnEvent", nil)
+end
+
+
+-- 
+-- Load "klnConfig" addon if it hasn't been already.
+-- Regardless, open the in-game addon configuration panel
+-- straight to the primary pane.
+-- 
+function LoadConfiguration()
+  if IsAddOnLoaded("klnConfig") then
+    klnConfig:Open()
+  end
+  local loaded, reason = LoadAddOn("klnConfig")
+  if not loaded then
+    error("Failed to load configuration: "..reason)
+  end
+  klnConfig:Open()
+end
